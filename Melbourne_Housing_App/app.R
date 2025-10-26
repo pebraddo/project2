9d08adfb557543c2900fe0f9945124585e658dc1
Powell Braddock <pebraddo@ncsu.edu>
2025-10-26 18:56:01 -0400

    designing the app

--- Melbourne_Housing_App/app.R
+++ Melbourne_Housing_App/app.R
@@ -1,54 +1,67 @@
 library(bslib)
 library(ggplot2)
 library(shiny)
+library(shinydashboard)
 
 # Define UI for application
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Melbourne Housing Application"),
-
-    
-    sidebarLayout(
-        sidebarPanel(
-            selectInput('var1', 
-                        label='Choose categorical variables', 
-                        c('Suburb', 'Method', 'Type', 'SellerG', 'Postcode', 'Regionname', 'CouncilArea'),
-                        selected = c('Suburb', 'Method'), 
-                        multiple=TRUE
-                        ),
-            selectInput('num_var',
-                        label='Choose a numeric variable',
-                        choices= c('Rooms', 'Price', 'Propertycount', 'Distance'),
-                        selected = 'Price'
-                        ),
-            sliderInput(
-              'range',
-              label= 'Range of interest:',
-              min=0,
-              max=6000000,
-              value=c(40000, 80000)
-            ),
-            actionButton('plot_it',
-                         label='Plot it!')
-        ),
-        
-
-        # Show a plot of the generated distribution
-        card(
-           plotOutput(outputId = 'scatterplot')
-        )
+ui <- dashboardPage(
+  dashboardHeader(title='Melbourne Housing Application'),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem('About', tabName = 'about_tab', icon=icon('archive')),
+      menuItem('Data Download', tabName='download_dat', icon=icon('laptop')),
+      menuItem('Data Exploration', tabName = 'explore_tab', icon=icon('laptop'))
+      ), 
+    conditionalPanel(selectInput('cat_var', 
+                                 label='Choose categorical variables', 
+                                 c('Suburb', 'Method', 'Type', 'SellerG', 'Postcode', 'Regionname', 'CouncilArea'),
+                                 selected = c('Suburb', 'Method'), 
+                                 multiple=TRUE),
+                     selectInput('num_var1',
+                                 label='Choose a numeric variable',
+                                 choices= c('Price', 'Propertycount'),
+                                 selected = 'Price'),
+                     sliderInput('range1',
+                                 label= 'Range of interest for Price or Propertycount:', 
+                                 min=0,max=1000000,value=c(2000, 10000)),
+                     selectInput('num_var2',
+                                 label='Choose another numeric variable',
+                                 choices= c('Rooms','Propertycount'),
+                                 selected = 'Rooms'),
+                     sliderInput('range2',
+                                 label='Range of Interest for Rooms or Distance:',
+                                 min = 1,max=10,value=c(1,4)),
+                     actionButton('plot_it',label='Plot it!')),
+    card(plotOutput(outputId = 'scatterplot'))),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = 'about_tab',
+              titlePanel('About the Data')
+              ),
+      tabItem(tabName = 'download_dat',
+              titlePanel('Data Download')
+              ),
+      tabItem(tabName = 'explore_tab',
+              titlePanel('Data Exploration'))
     )
+  )
 )
 
 
+
+
 server <- function(input, output) {
-  #re<- eventReactive(input$plot_it,
+  plot_reactive <-eventReactive(input$plot_it, 
+                                {melbourne_house_data})
+  output$scatterplot <- renderPlot({
+    scatterplot <- plot_reactive()
     
-    output$scatterplot <- renderPlot({
-      ggplot(data=melbourne_house_data, aes_string(x=input$var1[1], y=input$num_var, fill=input$var1[2])) +
-        geom_point() +ylim(input$range)
-    })#)
+    ggplot(scatterplot, aes_string(x=input$cat_var[1], 
+                                   y=input$num_var1)) +
+      geom_point() +
+      ylim(input$range1)
+    })
+    
 }
 
 # Run the application 
